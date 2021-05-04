e63ffe085d37a6472a3bf9deee341bc599990c6f
Dan <khazifire@gmail.com>
2021-05-04 12:59:06 +0700

    updated project page and landing page

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ hamburger.addEventListener('click', () => {
 document.addEventListener('scroll', () => {
 	var scroll_position = window.scrollY;
 	if (scroll_position > 260) {
-		header.style.backgroundColor = 'var(--secondary-background';
+		header.style.backgroundColor = 'var(--primary-background';
 	} else {
 		header.style.backgroundColor = 'transparent';
 	}
